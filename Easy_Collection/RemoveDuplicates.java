a5a70466a4d66a5956dcfb0b285b1088617ca908
albert <alertshin5172@gmail.com>
2025-07-17 20:06:34 +0900

    Add a Comment - Remove Duplicates from Sorted Array

--- Easy_Collection/RemoveDuplicates.java
+++ Easy_Collection/RemoveDuplicates.java
@@ -1,3 +1,11 @@
+/*
+Example 2:
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
         if(nums.length == 0) return 0;
@@ -14,7 +22,9 @@ public class RemoveDuplicates {
         RemoveDuplicates sol = new RemoveDuplicates();
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
         int length = sol.removeDuplicates(nums);
+        
         System.out.println("New length: " + length);
+
         for (int i = 0; i < length; i++) {
             System.out.print(nums[i] + " ");
         }
