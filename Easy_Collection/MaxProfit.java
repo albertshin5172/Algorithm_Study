6473e0de6f4a1d5a2782b26d7dd5456852ff9186
albert <alertshin5172@gmail.com>
2025-07-17 22:09:00 +0900

    refactor - Code structure cleanup

--- Easy_Collection/MaxProfit.java
+++ Easy_Collection/MaxProfit.java
@@ -10,7 +10,6 @@ Total profit is 4 + 3 = 7.
 public class MaxProfit {
     public int maxProfit(int[] prices) {
         int max = 0;
-        int length = prices.length;
 
         for (int i = 0; i < prices.length - 1; i++) {
             if (prices[i + 1] > prices[i]) {
