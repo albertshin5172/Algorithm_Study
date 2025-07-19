238fb7a6ef90132497c822744b8d134596fc0836
albertshin5172 <32608100+albertshin5172@users.noreply.github.com>
2025-07-19 19:51:20 +0900

    0 ms (100%) 41.7 MB (83.05%) 07-19-2025 0066-plus-one Easy .java

--- LeetCode/Easy/0066-plus-one/0066-plus-one.java
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.java
@@ -1,13 +1,18 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        
-        if(nums.length==1 || nums[0]!=nums[1]) return nums[0];
-        
-        for(int i=1; i<nums.length-1; i++){
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            // If the current digit is less than 9, simply add 1 and return.
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            //If the current digit is 9, set it to 0 and continue the loop.  
+            digits[i] = 0;
         }
-        
-        return nums[nums.length-1];
+        // If all digits were 9, create an extra space in a new array.
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1; // Put 1 in the first element, the rest should be 0
+        return newDigits;
     }
 }
