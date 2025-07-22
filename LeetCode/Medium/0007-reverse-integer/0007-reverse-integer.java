d77601f4b2ba2b765ae340409046933d94383c3e
albertshin5172 <32608100+albertshin5172@users.noreply.github.com>
2025-07-22 21:58:41 +0900

    Time: 1 ms (81.79%), Space: 40.9 MB (56.71%) - LeetHub

---
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int reverse(int x) {        
+        //It prevents exceeding the maximum or minimum value.
+        //This helps to detect overflow beyond the 32-bit integer range
+        long revNum =0;
+
+        // Repeat until all digits of x have been processed
+        while (x != 0) {
+            // Extract the last digit of x
+           
+            int tmpNum = x % 10;
+            // Get rid of the last digit from x
+            x /= 10;
+            //Append the extracted digit to revNum by shifting digits to the left and adding tmpNum
+            revNum = revNum*10 + tmpNum;
+        }
+        
+        //Overflow check
+        if(revNum > Integer.MAX_VALUE || revNum < Integer.MIN_VALUE) {
+            return 0;  //Integer.max_value handle long value
+        }
+        return (int) revNum;
+        
+    }
+}
