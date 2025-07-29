e5cc2c59892610482360b43e3135efdd90b9fb70
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-07-29 13:08:48 +0900

    Time: 25 ms (66.93%), Space: 40.3 MB (81.64%) - LeetHub

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.java
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.java
@@ -6,10 +6,17 @@ public class Solution extends VersionControl {
         int s = 1;
         int e = n;
         while(s <= e){
+            /*             
+            Calculate the middle index m between s and e
+            Using s + (e - s) / 2 to avoid integer overflow
+            m safely splits the search range in half             
+            */
             int m = s + (e-s)/2;
             if(isBadVersion(m)){
+                //if m is smaller then the end, update the end index
                 e = m - 1;
             }else{
+                //if m is bigger then the start, update the start index
                 s = m + 1;
             }
         }
