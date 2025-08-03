9baba43e5f4ddfa5e25c66e69d996698836e47a1
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-03 23:27:07 +0900

    Time: 0 ms (100%), Space: 44.4 MB (76.72%) - LeetHub

--- LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,18 +11,22 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        // Initialize slow and fast pointers at head
         ListNode fast = head;
         ListNode slow = head;
 
+        // Traverse while fast pointer and its next node are not null
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
+            fast = fast.next.next; // Move fast pointer by 2 nodes
+            slow = slow.next;      // Move slow pointer by 1 node
 
+            // If both pointers meet, a cycle exists
             if (fast == slow) {
                 return true;
             }
         }
 
-        return false;      
+        // If fast pointer reaches the end, no cycle exists
+        return false;   
     }
 }
