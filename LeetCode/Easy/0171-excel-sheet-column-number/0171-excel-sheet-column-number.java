c04a3c6fe7b36ebf39b12ea15d619fb31cc294b8
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-05 23:10:43 +0900

    Time: 1 ms (91.41%), Space: 42.3 MB (63.67%) - LeetHub

--- LeetCode/Easy/0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ LeetCode/Easy/0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,14 +1,13 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
         int result = 0;
-        int value = 0;
-        char c= '\0';
-        
+
         for(int i=0; i<columnTitle.length(); i++){
-            c = columnTitle.charAt(i); 
-            value = c - 'A' + 1;
+            char c= columnTitle.charAt(i); 
+            int value = c - 'A' + 1;
             result = result * 26 + value;
         }
+        
         return result;
     }
 }
