540d13fd13e890e78895c3e5dfae5728842e232c
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-07 15:01:27 +0900

    Time: 9 ms (99.37%), Space: 61.2 MB (8.29%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,23 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        /*
+        Sort the array
         Arrays.sort(nums);
         for(int i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1]){ 
-                return true; 
-            }
+            if(nums[i] == nums[i+1]) return true;
         }
+        
+        return false;   // If no duplicates found, return false
+        */
+        //Alternative solution solution using a HashSet to track visited numbers
+        Set<Integer> seen = new HashSet<>();
+        
+         // Iterate through each number in the array
+        for (int num : nums) {
+            if (!seen.add(num)) return true;    // if already exist, add will be false
+        }
+
+        // If no duplicates encountered, return false
         return false;
     }
 }
