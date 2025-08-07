2728b2c54c8c9c4eeb6d66b9e55561ed53e31829
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-07 15:48:48 +0900

    Time: 1 ms (99.85%), Space: 46.1 MB (60.32%) - LeetHub

--- LeetCode/Easy/0136-single-number/0136-single-number.java
+++ LeetCode/Easy/0136-single-number/0136-single-number.java
@@ -1,13 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        //complexity O(nlogn) and constant extra space O(1).
+        /*
         Arrays.sort(nums);
         
-        if(nums.length==1 || nums[0]!=nums[1]) return nums[0];
+        if(nums.length == 1 || nums[0] != nums[1]) return nums[0];
         
         for(int i=1; i<nums.length-1; i++){
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
+            if(nums[i-1] != nums[i] && nums[i] != nums[i+1]) return nums[i];
         }
         
         return nums[nums.length-1];
+        */
+        
+        int result = 0;
+        for(int num : nums){
+            // XOR properties
+            //complexity O(n) and constant extra space O(1).
+            result ^= num;
+        }
+        return result;
     }
 }
