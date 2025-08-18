2326dbe564e98f677a2a9982fe8651ce20204167
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-18 16:27:03 +0900

    Time: 20 ms (10.91%), Space: 48.7 MB (99.6%) - LeetHub

--- LeetCode/Easy/0169-majority-element/0169-majority-element.java
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.java
@@ -1,9 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //Sort the array so that the majority element will be positioned at the center.
-        //The key is The majority element is the element that appears more than ⌊n / 2⌋ times. 
-        Arrays.sort(nums);
-        //Returned the element located in the middle index of the array
-        return nums[nums.length/2];
+        //Time complexity: O(n)
+        //Space complexity: O(n)
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int majority = 0;
+        int result = 0;
+        
+        // Count the frequency of each element
+        // getOrDefault: if num exists, get its value; otherwise start at 0
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if(map.get(num) > majority){
+                result = num;
+                majority = map.get(num);
+            }
+        }
+
+        return result;
     }
 }
