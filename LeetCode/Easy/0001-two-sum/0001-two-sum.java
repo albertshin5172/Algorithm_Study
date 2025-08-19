ecf3c74c5571e9e77e3e6e2e9d6fba420cfec6e2
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-19 21:09:11 +0900

    Time: 3 ms (61.83%), Space: 45 MB (47.1%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,5 +1,24 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // HashMap to store number and its index
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Iterate through the array
+        for(int i=0; i<nums.length; i++){
+            int result = target - nums[i];
+            //check if the complement exists in the map
+            if(map.containsKey(result)){
+                //if exists, return the pair of indices
+                return new int[] { map.get(result), i };
+            }
+            //Otherwise, add current number and its index to the map
+            map.put(nums[i], i);
+        }
+        
+        // Return an empty array instead of null if no valid pair is found.
+        return new int[] {};
+        
+        /*
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
@@ -9,5 +28,6 @@ class Solution {
         }
         // Return an empty array instead of null if no valid pair is found.
         return new int[] {};
+        */
     }
 }
