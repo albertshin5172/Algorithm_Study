cc0ef5f6c57f648ae6f3721c7b750a98d07efa92
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-20 21:54:44 +0900

    Time: 0 ms (100%), Space: 49.7 MB (11.24%) - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
@@ -1,6 +1,8 @@
 class Solution {
     public void reverseString(char[] s) {
+        // Only traverse half of the array (since front/back are swapped simultaneously)
         for (int i = 0; i < s.length / 2; i++) {
+            // Swap the current position character with the symmetric position character
             char tmp = s[i];
             s[i] = s[s.length - 1 - i];
             s[s.length - 1 - i] = tmp;
