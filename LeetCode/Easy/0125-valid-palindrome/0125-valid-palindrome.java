8646cf268b071542f97b1b59569b79633d0684de
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-20 22:09:49 +0900

    Time: 2 ms (98.78%), Space: 42.9 MB (83.47%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        
+        while(left < right){
+            //If the character pointed to by the left pointer is not a number, move one space to the right.
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            //If the character pointed to by the right pointer is not a number, move one space to the left.
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+
+            //// Compare characters without case distinction and return false if they are different
+            if(Character.toUpperCase(s.charAt(left)) !=  Character.toUpperCase(s.charAt(right))) return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+        /*
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         StringBuffer sb = new StringBuffer(s);
         String reversedStr = sb.reverse().toString();
         
         if(s.equals(reversedStr)) return true;
         else return false;
+        */
     }
 }
