e32f6699a54f0dd03f82be02d371d65d6b7f4e4e
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-08-22 17:20:25 +0900

    Time: 1 ms (62.03%), Space: 41.9 MB (32%) - LeetHub

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.java
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,12 +1,27 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        /* Horizontal scanning */
+        /*
         String fStr = strs[0];
 
-        for(int i=1; i<strs.length; i++){
+        for(int i = 0; i<strs.length; i++){
             while(strs[i].indexOf(fStr) != 0){
-                fStr=fStr.substring(0,fStr.length()-1);
+                fStr = fStr.substring(0, fStr.length()-1);
             }
         }
         return fStr;
+        */
+
+        /* Vertical Scanning */
+        if(strs == null || strs.length == 0) return "";
+
+        for(int i=0; i<strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+
+            for(int j=1; j < strs.length; j++){
+                if(i == strs[j].length() || c != strs[j].charAt(i)) return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
     }
 }
