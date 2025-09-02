b8587c124b5c272c301c6c052b316c187ddedae8
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-09-02 19:16:33 +0900

    Time: 6 ms (98.43%), Space: 44.9 MB (59.96%) - LeetHub

--- LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,22 +1,45 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> freqs = new HashMap<>();
-        int res = 0, i = 0, maxFreq = 0;
+        /* Hash */
+        /*
+        HashMap<Character, Integer> count = new HashMap<>();
+        int result = 0;
+        int left = 0;
+        int maxFreq = 0;
+        
+        for(int right=0; right<s.length(); right++){
+            char c = s.charAt(right);
+            count.put(c, count.getOrDefault(c, 0) + 1);
 
-        for (int j = 0; j < s.length(); j++) {
-            char c = s.charAt(j);
-            freqs.put(c, freqs.getOrDefault(c, 0) + 1);
-            maxFreq = Math.max(maxFreq, freqs.get(c));
+            // update the most frequent character count in window
+            maxFreq = Math.max(maxFreq, count.get(c));
 
-            while ((j - i + 1) - maxFreq > k) {
-                char left = s.charAt(i);
-                freqs.put(left, freqs.get(left) - 1);
-                i++;
+            // if replacements needed > k, shrink window
+            while((right-left+1) - maxFreq > k ){
+                char leftChar = s.charAt(left);
+                count.put(leftChar, count.get(leftChar)-1);
+                left++;
             }
-
-            res = Math.max(res, j - i + 1);
+            // update result with valid window length
+            result = Math.max(result, right-left + 1);
         }
+        return result;
+        */
+        /* Array */
+        int[] count = new int[26];
+        int left = 0, maxFreq = 0, result = 0;
 
-        return res;
+        for(int right=0; right<s.length(); right++){
+            int idx = s.charAt(right)-'A';
+            count[idx]++;
+            maxFreq = Math.max(maxFreq, count[idx]);
+
+            while(right-left+1 - maxFreq > k){
+                count[s.charAt(left)-'A']--;
+                left++;
+            }
+            result = Math.max(result, right-left+1);
+        }
+        return result;
     }
 }
