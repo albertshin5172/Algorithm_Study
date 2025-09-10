ca5c2982d610e22a6a2beefe230d55a50a652487
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-09-10 18:38:07 +0900

    Time: 22 ms (52.9%), Space: 59.5 MB (75.08%) - LeetHub

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,25 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-         Set<Integer> window = new HashSet<>();
+        // Check if the current number already exists in the window
+        // If yes, it means a duplicate exists within distance k
+        Set<Integer> window = new HashSet<>();
 
-         for(int i=0; i<nums.length; i++){
+        // Iterate through the array
+        for(int i=0; i<nums.length; i++){
+            // Check if the current number already exists in the window
+            // If yes, it means a duplicate exists within distance k
             if(window.contains(nums[i])){
                 return true;
             }
-            window.add(nums[i]);
 
-            if(window.size()>k){
+            // Add the current number to the window
+            window.add(nums[i]);
+            // If the size of the window exceeds k, remove the oldest element
+            // This maintains the window size to at most k elements
+            if(window.size() > k){
                 window.remove(nums[i-k]);
             }
-
-         }
-         return false;
+        }
+        return false;
     }
 }
