dbcaab77326957c20e6187b16256ff4abade181f
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-09-12 00:02:45 +0900

    Time: 2 ms (99.9%), Space: 56.3 MB (54.78%) - LeetHub

--- LeetCode/Easy/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ LeetCode/Easy/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,12 +1,12 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-      long sum = 0;                   // 안전하게 long 사용
-      for (int i = 0; i < k; i++) sum += nums[i];   // 초기 윈도우
-      long max = sum;
+        long sum = 0;
+        for(int i=0; i<k; i++) sum += nums[i];
+        long max = sum;
 
-        for(int i=k; i < nums.length; i++){
+        for(int i=k; i<nums.length; i++){
             sum += nums[i] - nums[i-k];
-             if(sum > max ) max = sum;
+            max = Math.max(sum, max);
         }
         return max/(double)k;
     }
