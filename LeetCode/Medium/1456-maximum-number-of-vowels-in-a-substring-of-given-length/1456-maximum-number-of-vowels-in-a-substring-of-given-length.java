898c0d59df609fa36f28b3f83d97030663c08f54
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-09-17 00:23:37 +0900

    Time: 18 ms (52.49%), Space: 44.5 MB (85.12%) - LeetHub

--- LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -4,21 +4,20 @@ class Solution {
         int count = 0, maxCount = 0;
 
         // 1. Initial window (the first k characters)
-        for (int i = 0; i < k; i++) {
-            if (vowels.indexOf(s.charAt(i)) != -1) count++;
+        for(int i=0; i<k; i++){
+            if(vowels.indexOf(s.charAt(i)) != -1 ) count++;
         }
-        maxCount = count;
 
-        // 2. Sliding window
-        for (int i = k; i < s.length(); i++) {
+        maxCount = count;
+         // 2. Use a sliding window approach
+        for(int i=k; i<s.length(); i++){
             // New character on the right
-            if (vowels.indexOf(s.charAt(i)) != -1) count++;
+            if(vowels.indexOf(s.charAt(i)) != -1 ) count++;
             // Missing character on the left
-            if (vowels.indexOf(s.charAt(i - k)) != -1) count--;
+            if(vowels.indexOf(s.charAt(i-k)) != -1 ) count--;
             // Update the maximum
             maxCount = Math.max(maxCount, count);
         }
-
         return maxCount;
     }
 }
