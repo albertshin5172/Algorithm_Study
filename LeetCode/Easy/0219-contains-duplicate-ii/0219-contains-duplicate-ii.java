7d6b919c58c17a556ee7553e6e307625efdd95c4
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-09-22 23:43:18 +0900

    Time: 22 ms (52.49%), Space: 59.7 MB (54.61%) - LeetHub

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,20 +3,18 @@ class Solution {
         // Use a HashSet to represent the sliding window of size k
         Set<Integer> window = new HashSet<>();
 
-        for (int i = 0; i < nums.length; i++) {
+        for(int i=0; i<nums.length; i++){
             // If the current number already exists in the window,
             // then we have found a duplicate within distance k
-            if (window.contains(nums[i])) return true;
+            if(window.contains(nums[i])) return true;
 
             // Add the current number to the window
             window.add(nums[i]);
-
+            
             // Maintain the window size: remove the element that is out of range (i - k)
-            if (window.size() > k) {
-                window.remove(nums[i - k]);
-            }
+            if(window.size() > k)   window.remove(nums[i-k]);
         }
-
+        
         // If no duplicates found within distance k, return false
         return false;
     }
