d5378c943a2fe39a1cec242a2b8ba4142267f41a
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-09-25 22:40:37 +0900

    Time: 8 ms (56.76%), Space: 42.6 MB (90.52%) - LeetHub

--- LeetCode/Medium/0567-permutation-in-string/0567-permutation-in-string.java
+++ LeetCode/Medium/0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,29 +1,60 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        /*
         int m = s1.length();
         int n = s2.length();
-        if (m > n) return false;
-        
-        int[] need  = new int[26];
-        int[] window  = new int[26];
+        if(m>n) return false;
 
-        // build need and initial window
+        int[] need = new int[26];
+        int[] window = new int[26];
+
         for(int i=0; i<m; i++){
             need[s1.charAt(i)-'a']++;
             window[s2.charAt(i)-'a']++;
         }
 
-        if (Arrays.equals(need, window)) return true;
+        if(Arrays.equals(need, window)) return true;
 
-        // slide window over s2
-        for (int i = m; i < s2.length(); i++) {
-            // add s2[i], remove s2[i-m]
+        for(int i=m; i<n; i++){
+            window[s2.charAt(i)-'a']++;
+            window[s2.charAt(i-m)-'a']--;
+
+            if(Arrays.equals(need, window)) return true;
+        }
+        return false;
+        */
+        int m = s1.length();
+        int n = s2.length();
+        if (m > n) return false;
+
+        int[] need = new int[26];
+        int[] window = new int[26];
+        for (int i = 0; i < m; i++) {
+            need[s1.charAt(i) - 'a']++;
             window[s2.charAt(i) - 'a']++;
-            window[s2.charAt(i - m) - 'a']--;
-
-            if (Arrays.equals(need, window)) return true;
         }
 
+        int matches = 0;
+        for (int i = 0; i < 26; i++) if (need[i] == window[i]) matches++;
+
+        if (matches == 26) return true;
+
+        for (int r = m; r < n; r++) {
+            int add = s2.charAt(r) - 'a';
+            int remove = s2.charAt(r - m) - 'a';
+
+            // add char r
+            if (window[add] == need[add]) matches--; // equality will break
+            window[add]++;
+            if (window[add] == need[add]) matches++; // equality achieved
+
+            // remove char r-m
+            if (window[remove] == need[remove]) matches--; // equality will break
+            window[remove]--;
+            if (window[remove] == need[remove]) matches++; // equality achieved
+
+            if (matches == 26) return true;
+        }
         return false;
     }
 }
