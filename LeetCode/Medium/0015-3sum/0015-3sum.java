34e931d1fc16092003433a3d60afcefbee4fb463
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-09-28 21:20:57 +0900

    Time: 31 ms (56.71%), Space: 51.8 MB (39.63%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,28 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // Sort the array to make it easier to avoid duplicates and use two-pointer approach
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
 
-        for(var i=0; i<nums.length-2; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            int left = i + 1;
-            int right = nums.length-1;
+        // Fix one number and find the other two using two pointers
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values to avoid duplicate triplets
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while(left<right){
+            int left = i + 1;              // Start pointer after the fixed number
+            int right = nums.length - 1;   // End pointer at the last element
+
+            // Use two pointers to find pairs that sum up with nums[i] to zero
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if(sum == 0){
+                if (sum == 0) {
+                    // A valid triplet found, add it to the result
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-                }else if(sum < 0){
+
+                    // Skip duplicate values for the second element
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    // Skip duplicate values for the third element
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
+                } else if (sum < 0) {
+                    // Increase the sum by moving left pointer to the right
                     left++;
-                }else { 
-                    right--; 
+                } else {
+                    // Decrease the sum by moving right pointer to the left
+                    right--;
                 }
             }
         }
 
+        // Return the list of all unique triplets
         return result;
     }
 }
