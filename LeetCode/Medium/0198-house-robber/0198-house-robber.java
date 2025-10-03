41b8e457c9cb662621e840dad9f28c90272adf8c
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-03 18:50:42 +0900

    0 ms (100%) 41.5 MB (8.7%) 10-03-2025 0198-house-robber Medium .java

--- LeetCode/Medium/0198-house-robber/0198-house-robber.java
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.java
@@ -2,26 +2,17 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
 
-        // If there is only one house, the maximum money is just nums[0]
         if(n==1) return nums[0];
 
-        // dp[i] represents the maximum amount of money that can be robbed 
-        // from the first i+1 houses
         int[] dp = new int[n];
 
-        // Initialize the first two houses
-        dp[0] = nums[0]; // Only one option: rob house 0
-        dp[1] = Math.max(nums[0], nums[1]); // Rob the house with the larger amount
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        // Fill dp array for the rest of the houses
-        for(int i = 2; i < n; i++) {
-            // Two choices:
-            // 1) Skip current house -> dp[i-1]
-            // 2) Rob current house -> nums[i] + dp[i-2]
-            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
+        for(int i=2; i<n; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
         }
-        
-        // The last element contains the maximum amount of money that can be robbed
+
         return dp[n-1];
     }
 }
