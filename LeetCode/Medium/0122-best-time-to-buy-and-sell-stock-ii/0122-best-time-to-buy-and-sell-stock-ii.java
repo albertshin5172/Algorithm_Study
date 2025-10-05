8f1832fce9214de8e7a1c410e00f6f6083b20d74
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-05 12:36:02 +0900

    4 ms (5.58%) 45.9 MB (48.08%) 10-05-2025 0122-best-time-to-buy-and-sell-stock-ii Medium .java

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0;
+        int n = prices.length;
+        int[][] dp = new int[n][2];
 
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i + 1] > prices[i]) {
-                max = max + (prices[i + 1] - prices[i]);
-            }
+        dp[0][0] = 0;           // Not holding any stock on day 0
+        dp[0][1] = -prices[0];  // Holding a stock bought on day 0
+
+        for (int i = 1; i < n; i++) {
+            // Max profit today without holding: either keep not holding or sell today
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+
+            // Max profit today while holding: either keep holding or buy today
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
         }
 
-        return max;
+        // Max profit at the end of the last day when not holding any stock
+        return dp[n-1][0];
     }
 }
