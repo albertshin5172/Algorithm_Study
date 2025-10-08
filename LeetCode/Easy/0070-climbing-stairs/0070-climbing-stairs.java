58f2c1ee8f96f06035920b86584877cf0b789fcb
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-08 23:02:47 +0900

    0 ms (100%) 41 MB (5.79%) 10-08-2025 0070-climbing-stairs Easy .java

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,5 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
+        /*
         if(n ==1 || n==2) return n;
 
         int [] arr = new int [n];
@@ -10,5 +11,22 @@ class Solution {
             arr[i] = arr[i-1]+arr[i-2];
         }
         return arr[n-1];
+        */
+        //DP array memory optimization (using only two variables)
+        // Initialize 1st value, 2nd value
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int first = 1;  // Ways to climb to step 1 (f(1))
+        int second = 2; // Ways to climb to step 2 (f(2))
+        int result = 0;
+
+        // Repeat from the 3rd to the nth time
+        for (int i = 3; i <= n; i++) {
+            result = first + second; // f(n) = f(n-1) + f(n-2)
+            first = second;          //Move one space (update the previous value)
+            second = result;         //Move one space (update the latest value)
+        }
+        return result;
     }
 }
