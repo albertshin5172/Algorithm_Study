6b15eefcac1d509371b74162377c0540f6a108c3
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-08 23:04:53 +0900

    N/A (0%) N/A (0%) 10-08-2025 0070-climbing-stairs Easy .java

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,19 +2,19 @@ class Solution {
     public int climbStairs(int n) {
         // Base cases: if there is only 1 or 2 steps, 
         // the number of ways to climb is equal to n itself
-        if(n == 1 && n == 2) {
+        if(n == 1 || n == 2) {
             return n;
         }
        
 
         // Create an array to store the number of ways to climb each step
-        int[] dp = new int[n];
+        int[] dp = new int[n+1];
 
         // Initialize the first two steps
         // Only 1 way to climb 1 step
         // Two ways to climb 2 steps (1+1 or 2)
         dp[0] = 1;
-        dp[1] = 1;
+        dp[1] = 2;
 
         // Compute the number of ways for steps 3 to n
         // Each step i can be reached either from step (i-1) or (i-2)
@@ -24,6 +24,6 @@ class Solution {
         }
 
         // Return the number of ways to climb n steps
-        return dp[n-1];
+        return dp[n];
     }
 }
