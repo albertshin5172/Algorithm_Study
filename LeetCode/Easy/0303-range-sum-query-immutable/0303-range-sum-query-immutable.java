45b590a5d9dfe41c29c10b3f6d6e74e0399dbc22
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-12 20:41:14 +0900

    7 ms (100%) 49.1 MB (95.43%) 10-12-2025 0303-range-sum-query-immutable Easy .java

--- LeetCode/Easy/0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ LeetCode/Easy/0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,14 +1,23 @@
 class NumArray {
+    // Stores the prefix sums of the input array nums.
     private int[] prefixSum;
 
+    /**
+     * Constructor: Computes the prefix sum array for O(1) range sum queries.
+     */
     public NumArray(int[] nums) {
+        // prefixSum[0] = 0. prefixSum[i] = sum of nums[0]...nums[i-1].
         prefixSum = new int[nums.length+1];
-        prefixSum[0] = 0;
+
         for(int i=1; i<prefixSum.length; i++){
             prefixSum[i] = prefixSum[i-1] + nums[i-1];
-        }  
+        }
     }
     
+    /**
+     * Returns the sum of elements in nums between indices left and right (inclusive).
+     * Calculation: prefixSum[right + 1] - prefixSum[left]
+     */
     public int sumRange(int left, int right) {
         return prefixSum[right+1] - prefixSum[left];
     }
