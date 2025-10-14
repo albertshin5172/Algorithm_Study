4e91027440548991b8d2aab4a4864941027bb4fa
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-14 21:54:09 +0900

    0 ms (100%) 40.4 MB (62.93%) 10-14-2025 0062-unique-paths Medium .java

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,30 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        // Create a 2D DP array where dp[i][j] represents
+        // the number of unique paths to reach cell (i, j)
         int[][] dp = new int[m][n];
 
-        for(int i=0; i<m; i++){
-            dp[i][0] = 1; 
+        // Initialize the first column: there's only one way
+        // to reach any cell in the first column (by moving down)
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
 
-        for(int j=0; j<n; j++){
-            dp[0][j] = 1; 
+        // Initialize the first row: there's only one way
+        // to reach any cell in the first row (by moving right)
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
         }
 
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] += dp[i-1][j] + dp[i][j-1];
-            }    
+        // Fill the rest of the DP table
+        // Each cell can be reached either from the top or from the left
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
         }
 
-        return dp[m-1][n-1];
+        // The bottom-right cell contains the total number of unique paths
+        return dp[m - 1][n - 1];
     }
 }
