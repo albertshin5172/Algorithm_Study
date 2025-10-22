b7916ca70bbd719dbf72d4ee24e53a95579c1f36
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-22 23:07:45 +0900

    3 ms (82.45%) 45.2 MB (95.75%) 10-22-2025 0120-triangle Medium .java

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,21 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int [n][n];
+        int[] dp = new int[n];
 
+        // 1. Initialize dp with the values from the last row of the triangle
         for(int i=0; i<n; i++){
-            dp[n-1][i] = triangle.get(n-1).get(i);
+            dp[i] = triangle.get(n-1).get(i);
         }
-
+        // 2. Update dp array from bottom to top
+        // Each dp[j] stores the minimum path sum from triangle[i][j] to the bottom
         for(int i=n-2; i>=0; i--){
-            for(int j=i; j>=0; j--){
-                int left = triangle.get(i).get(j) + dp[i+1][j];
-                int right = triangle.get(i).get(j) + dp[i+1][j+1];
-
-                dp[i][j] = Math.min(left, right);
+            for(int j=0; j<=i; j++){
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
 
-        return dp[0][0];
+        // 3. dp[0] now contains the minimum path sum from the top to the bottom
+        return dp[0];
     }
 }
