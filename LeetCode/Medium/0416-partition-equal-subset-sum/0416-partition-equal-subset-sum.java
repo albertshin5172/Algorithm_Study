1305fa2595c8e74085a4e9a40596a2b00dc4418f
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-26 22:38:47 +0900

    30 ms (73.25%) 41.8 MB (98.57%) 10-26-2025 0416-partition-equal-subset-sum Medium .java

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,18 +5,18 @@ class Solution {
         for (int num : nums) total += num;
 
         // If the total sum is odd, we cannot split it into two equal subsets
-        if (total % 2 != 0) return false;
+        if (total%2 != 0) return false;
 
-        int target = total / 2; // Each subset should sum up to target
-        boolean[] dp = new boolean[target + 1];
+        int target = total/2; // Each subset should sum up to target
+        boolean[] dp = new boolean[target+1];
         dp[0] = true; // Base case: sum 0 is always possible (by taking no elements)
 
         // For each number in the array
         for (int num : nums) {
             // Iterate backwards to avoid reusing the same number multiple times
-            for (int i = target; i >= num; i--) {
+            for (int i=target; i>=num; i--) {
                 // If sum (i - num) is possible, then sum i is also possible
-                dp[i] = dp[i] || dp[i - num];
+                dp[i] = dp[i] || dp[i-num];
             }
         }
 
