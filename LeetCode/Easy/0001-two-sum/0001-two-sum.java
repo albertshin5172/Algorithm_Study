bc91b8b78d59be98eb79bf1a0cd9acca9b8e9905
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-10-30 23:05:32 +0900

    3 ms (59.27%) 45 MB (49.54%) 10-30-2025 0001-two-sum Easy .java

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,21 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        /*
-        // Iterate through each element in the array
-        for (int i = 0; i < nums.length; i++) {
-            // Compare the current element with all elements after it
-            for (int j = i + 1; j < nums.length; j++) {
-                // Check if the two elements add up to the target
-                if (nums[j] == target - nums[i]) {
-                    // Return the indices of the two elements
-                    return new int[] { i, j };
-                }
-            }
-        }
-        // If no pair is found, return an empty array instead of null
-        return new int[] {};
-        */
-        
+    public int[] twoSum(int[] nums, int target) {        
         // HashMap to store number and its index
         Map<Integer, Integer> map = new HashMap<>();
 
