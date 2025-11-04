2dd8256e161ebdd85306b8315e96ca302e793b0f
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-11-04 19:56:28 +0900

    5 ms (77.76%) 57.8 MB (73.2%) 11-04-2025 0011-container-with-most-water Medium .java

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,23 @@
 class Solution {
     public int maxArea(int[] height) {
         int len = height.length;
-        int left = 0;
-        int right = len-1;
-        int maxContainer = 0;
+        int left = 0;                // Pointer at the start of the array
+        int right = len-1;           // Pointer at the end of the array
+        int maxContainer = 0;        // Variable to store the maximum 
 
+        // Move pointers towards each other until they meet
         while(left < right){
+            // Calculate the current container area with width * min height of two pointers
             int currContainer = (right - left) * Math.min(height[left], height[right]);
+            // Update maximum area if the current area is larger
             maxContainer = Math.max(maxContainer, currContainer);
 
+            // Move the pointer pointing to the shorter line inward
             if(height[left] > height[right]) right--;
             else left++;
         }
+
+        // Return the largest area found
         return maxContainer;
     }
 }
