a151cf751aa50fb87405ca25caa186de00c05abb
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-11-07 07:41:46 +0900

    2 ms (79.81%) 46.1 MB (72.39%) 11-07-2025 0283-move-zeroes Easy .java

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        //The keypoint is O(n) / in-place / one-pass
+        //Position where non-zero elements will be placed
+        int idx = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            // Swap non-zero elements forward
+            if(nums[i] != 0){
+                int temp = nums[idx];
+                nums[idx] = nums[i];
+                nums[i] = temp;
+                idx++;
+            }
+        }
+        
+        /*
         int idx = 0;
         int zero = 0;
         for(int i=0; i<nums.length; i++){
@@ -13,5 +28,6 @@ class Solution {
         for(int i=0; i<zero; i++){
             nums[nums.length-1-i] = 0;
         }
+        */
     }
 }
