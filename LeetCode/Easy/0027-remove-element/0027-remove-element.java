bda208e57af6f589c6f421881e380611ffdb823d
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2025-11-08 20:47:55 +0900

    0 ms (100%) 43.4 MB (6.62%) 11-08-2025 0027-remove-element Easy .java

--- LeetCode/Easy/0027-remove-element/0027-remove-element.java
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.java
@@ -1,14 +1,21 @@
 class Solution {
+    // Removes all instances of 'val' from the array 'nums' in-place.
+    // Returns the new length of the array after removal.
     public int removeElement(int[] nums, int val) {
+        // If the array is empty, return 0
         if(nums.length == 0) return 0;
 
-        int cnt = 0;
+        int cnt = 0; // Pointer for the position of the next valid element
+        // Iterate through the array
         for(int i=0; i<nums.length; i++){
+            // If current element is not equal to 'val'
             if(nums[i]!=val){
+                // Place the valid element at 'cnt' position
                 nums[cnt]=nums[i];
-                cnt++;
+                cnt++; // Move pointer forward
             }
         }
+        // Return the count of valid elements (new length)
         return cnt;
     }
 }
