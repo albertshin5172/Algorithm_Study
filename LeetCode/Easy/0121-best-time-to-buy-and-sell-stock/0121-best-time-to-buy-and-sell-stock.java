01be56002c9b3d0fce737ddcb22e08d09da74d9c
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-12 23:51:56 +0900

    2 ms (42.01%) 62.2 MB (99.81%) 01-12-2026 0121-best-time-to-buy-and-sell-stock Easy .java

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[][] dp = new int[n][2];
-
-        // Initialize base case
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0]; 
-
-        for (int i = 1; i < n; i++) {
-            // Either keep not holding or sell the stock today
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]+prices[i]);
-            
-            // Either keep holding or buy the stock today
-            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+        int max = 0;
+        int min = prices[0];
+        
+        for(int i=1; i<prices.length; i++){
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i]-min);
         }
-
-        // The maximum profit is when not holding a stock on the last day
-        return dp[n-1][0];
+        return max;
     }
 }
