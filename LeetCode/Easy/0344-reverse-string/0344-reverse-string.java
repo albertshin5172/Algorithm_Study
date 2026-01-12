8b8eb77d1055f08a1eb9cc9507b9b8666746120a
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-13 00:20:39 +0900

    N/A (0%) N/A (0%) 01-13-2026 0344-reverse-string Easy .java

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.java
@@ -1,11 +1,15 @@
 class Solution {
     public void reverseString(char[] s) {
+        /*
+          This is a two-pointer method that accesses both the front and back indices simultaneously. 
+		  The back pointer is calculated as length - 1 - i.
+        */
         // Only traverse half of the array (since front/back are swapped simultaneously)
-        for (int i = 0; i < s.length / 2; i++) {
+        for(int i=0; i<s.length/2; i++){
             // Swap the current position character with the symmetric position character
-            char tmp = s[i];
-            s[i] = s[s.length - 1 - i];
-            s[s.length - 1 - i] = tmp;
+            char tmo = s[i];
+            s[i] = s[s.length-i-1];
+            s[s.length-i-1] = tmp; 
         }
     }
 }
