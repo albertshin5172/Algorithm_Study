3a4054aa0016b0b694aea1a425408a7e24c300fe
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-14 21:00:16 +0900

    32 ms (74.5%) 58.9 MB (89.82%) 01-14-2026 0015-3sum Medium .java

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -5,33 +5,34 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
 
         // Iterate through the array, fixing one element at a time
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i=0; i<nums.length-2; i++) {
             // Skip duplicate elements for the first number
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i>0 && nums[i] == nums[i-1]) continue;
 
-            int left = i + 1;               // Pointer to the next element
-            int right = nums.length - 1;    // Pointer to the last element
+            int left = i+1;               // Pointer to the next element
+            int right = nums.length-1;    // Pointer to the last element
 
             // Use two pointers to find pairs that sum with nums[i] to zero
-            while (left < right) {
+            while (left<right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum == 0) {
+                if (sum==0) {
                     // Found a triplet that sums to zero
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
 
                     // Skip duplicates for the second and third numbers
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < 0) {
+                    while (left<right && nums[left] == nums[left-1]) left++;
+                    while (left<right && nums[right] == nums[right+1]) right--;
+                } else if (sum<0) {
                     left++; // Move left pointer right to increase the sum
                 } else {
                     right--; // Move right pointer left to decrease the sum
                 }
             }
         }
+
         return result;
     }
 }
