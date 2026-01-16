a29cb8820c907ea21c375a95f60f100a03c435f9
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-16 11:53:19 +0900

    18 ms (86.54%) 44.1 MB (99.96%) 01-16-2026 0018-4sum Medium .java

--- LeetCode/Medium/0018-4sum/0018-4sum.java
+++ LeetCode/Medium/0018-4sum/0018-4sum.java
@@ -2,28 +2,29 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        
+
         for(int i=0; i<nums.length-3; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue; //Remove dupulication
+            if(i>0 && nums[i] == nums[i-1]) continue;       //Remove dupulication
 
             for(int j=i+1; j<nums.length-2; j++){
-                  if(j>i+1 && nums[j] == nums[j-1]) continue; //Remove dupulication
+                if(j>i+1 && nums[j] == nums[j-1]) continue;    //Remove dupulication
 
-                  int left = j+1;
-                  int right = nums.length -1;
+                int left = j+1;
+                int right = nums.length-1;
 
-                  while(left < right){
-                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                while(left<right){
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
 
                     if(sum == target){
-                        result.add(Arrays.asList(nums[i],nums[j], nums[left], nums[right]));
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         left++;
                         right--;
+                        
                         while(left<right && nums[left] == nums[left-1]) left++;
                         while(left<right && nums[right] == nums[right+1]) right--;
                     }else if(sum < target) left++;
                     else right--;
-                  }
+                }
             }
         }
         return result;
