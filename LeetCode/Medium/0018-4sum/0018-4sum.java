cb20b88b7ea904614a7d48e69b91c56cd307a139
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-16 11:53:25 +0900

    22 ms (22.62%) 45.8 MB (52.96%) 01-16-2026 0018-4sum Medium .java

--- LeetCode/Medium/0018-4sum/0018-4sum.java
+++ LeetCode/Medium/0018-4sum/0018-4sum.java
@@ -4,10 +4,10 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
 
         for(int i=0; i<nums.length-3; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;       //Remove dupulication
+            if(i>0 && nums[i] == nums[i-1]) continue;
 
             for(int j=i+1; j<nums.length-2; j++){
-                if(j>i+1 && nums[j] == nums[j-1]) continue;    //Remove dupulication
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
 
                 int left = j+1;
                 int right = nums.length-1;
@@ -19,14 +19,16 @@ class Solution {
                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         left++;
                         right--;
-                        
+
                         while(left<right && nums[left] == nums[left-1]) left++;
                         while(left<right && nums[right] == nums[right+1]) right--;
                     }else if(sum < target) left++;
                     else right--;
+                    
                 }
             }
         }
+
         return result;
     }
 }
