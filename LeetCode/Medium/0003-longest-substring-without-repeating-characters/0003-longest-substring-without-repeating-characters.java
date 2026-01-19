af22b03ca5fa46c0dc2905f6cd2ebe1586b22252
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-19 22:01:36 +0900

    6 ms (67.41%) 46.4 MB (49.1%) 01-19-2026 0003-longest-substring-without-repeating-characters Medium .java

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,25 +4,19 @@ class Solution {
         int maxLen = 0;                             // To store the length of longest substring without repeating characters
         Set<Character> window = new HashSet<>();    // Set to track characters currently in the window
 
-        // Loop through each character with right pointer
         for(int right=0; right<s.length(); right++){
-            char c = s.charAt(right);   // Current character at right pointer
+            char c = s.charAt(right);
 
-            // If the character already exists in the window,
-            // shrink the window from the left until duplicate is removed
             while(window.contains(c)){
                 window.remove(s.charAt(left));
-                left++; // Move the left pointer forward
+                left++;
             }
 
-            // Add the current character to the window
             window.add(c);
 
-            // Update the result with the maximum window size found so far
             maxLen = Math.max(maxLen, right-left+1);
         }
         
-        // Return the length of the longest substring without repeating characters
         return maxLen;
     }
 }
