df293623ee7ec0d615fccc4da3235c4d5c57b96f
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-22 12:20:22 +0900

    5 ms (99.58%) 47.2 MB (5.29%) 01-22-2026 1456-maximum-number-of-vowels-in-a-substring-of-given-length Medium .java

--- LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxVowels(String s, int k) {
+        /*
         String vowels = "aeiou";
         int cnt = 0, maxCnt = 0;
 
@@ -14,10 +15,38 @@ class Solution {
         for(int i=k; i<s.length(); i++){
             // New character on the right
             if(vowels.indexOf(s.charAt(i)) != -1) cnt++;
-            // Missing character on the left
+             // Missing character on the left
             if(vowels.indexOf(s.charAt(i-k)) != -1) cnt--;
             // Update the maximum
             maxCnt = Math.max(maxCnt, cnt);
+        }
+
+        return maxCnt;
+        */
+
+        boolean[] isVowel = new boolean[128];
+        isVowel['a'] = isVowel['e'] = isVowel['i'] = isVowel['o'] = isVowel['u'] = true;
+
+        char[] chr = s.toCharArray();
+        int len = chr.length;
+        int cnt = 0;
+
+        for(int i=0; i<k; i++){
+            if(isVowel[chr[i]]) cnt++;
+        }
+        
+        int maxCnt = cnt;
+        
+        for(int i=k; i<len; i++){
+            if(isVowel[chr[i]]) cnt++;
+
+            if(isVowel[chr[i-k]]) cnt--;
+
+            if (cnt > maxCnt) {
+                maxCnt = cnt;
+
+                if (maxCnt == k) return k;
+            }
         }
         
         return maxCnt;
