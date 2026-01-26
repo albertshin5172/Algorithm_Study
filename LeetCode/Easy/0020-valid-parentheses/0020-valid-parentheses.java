c3adf1ddccf34e7b010f24ae2164986d725b2875
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-26 22:12:41 +0900

    2 ms (97.11%) 41.9 MB (99.98%) 01-26-2026 0020-valid-parentheses Easy .java

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,11 +4,6 @@ class Solution {
 
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            /*
-            Make sure to use single quotes (e.g., ch == '(') when comparing char values.
-            Otherwise, get a compile-time error because comparing a char to a String. 
-            Incompatible operand types char and String
-            */
             if(ch == '(' || ch == '[' || ch == '{'){
                 stack.push(ch);
             }else {
