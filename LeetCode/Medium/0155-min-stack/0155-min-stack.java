247a7544e1b3fb04f1d7cac78984551b23cfc726
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-01-27 23:49:59 +0900

    5 ms (83.61%) 47 MB (76.95%) 01-27-2026 0155-min-stack Medium .java

---
+++ LeetCode/Medium/0155-min-stack/0155-min-stack.java
@@ -0,0 +1,57 @@
+/**
+ * Design a stack that supports push, pop, top, and retrieving 
+ * the minimum element in constant time.
+ */
+class MinStack {
+    // Main stack to store all elements
+    private Stack<Integer> stack;
+    // Auxiliary stack to store the minimum value at each level
+    private Stack<Integer> minStack;
+
+    /** * Initialize your data structure here. 
+     */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    /**
+     * Pushes the element val onto the stack.
+     */
+    public void push(int val) {
+        stack.push(val);
+        
+        // If minStack is empty, the first value is the minimum.
+        // Otherwise, push the smaller of the new value and the current minimum.
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            int currentMin = minStack.peek();
+            minStack.push(Math.min(val, currentMin));
+        }
+    }
+
+    /**
+     * Removes the element on the top of the stack.
+     */
+    public void pop() {
+        // Pop from both stacks to keep them synchronized
+        stack.pop();
+        minStack.pop();
+    }
+
+    /**
+     * Gets the top element of the stack.
+     */
+    public int top() {
+        return stack.peek();
+    }
+
+    /**
+     * Retrieves the minimum element in the stack.
+     */
+    public int getMin() {
+        // The top of minStack always represents the minimum of the main stack
+        return minStack.peek();
+    }
+}
