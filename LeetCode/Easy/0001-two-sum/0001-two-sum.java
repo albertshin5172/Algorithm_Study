5ec25aa64a672e002babd1acb5b60205acf35145
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-02-04 01:04:00 +0900

    3 ms (59.44%) 47.4 MB (18.2%) 02-04-2026 0001-two-sum Easy .java

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -2,14 +2,15 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int result = target - nums[i];
 
-            if(map.containsKey(result)){
-                return new int[]{map.get(result), i};
+            if (map.containsKey(result)) {
+                return new int[] { map.get(result), i };
             }
             map.put(nums[i], i);
         }
+
         return new int[] {};
     }
 }
