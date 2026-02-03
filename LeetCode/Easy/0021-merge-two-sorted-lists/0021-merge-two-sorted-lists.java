93be1d9e8898d5d7210a6e8c5987cc774434f78c
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-02-04 00:54:27 +0900

    0 ms (100%) 44.6 MB (16.43%) 02-04-2026 0021-merge-two-sorted-lists Easy .java

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,30 +10,27 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        /* 
-        Dummy nodes serve as a useful tool for simplifying code by minimizing special cases when inserting 
-        or deleting nodes in a linked list. They allow operations to be handled uniformly, 
-        without having to treat the head node differently.  
-        */
-        ListNode dummy = new ListNode(-1);
-        ListNode mList=dummy;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
-        while(list1 !=null && list2 !=null){
-            if(list1.val<list2.val){
-                mList.next = list1;
+        while(list1 != null && list2 !=null){
+            if(list1.val < list2.val){
+                current.next = list1;
                 list1 = list1.next;
             }else{
-                mList.next = list2;
+                current.next = list2;
                 list2 = list2.next;
             }
-            mList = mList.next;
+            current = current.next;
         }
+
         if(list1 == null){
-            mList.next = list2;
+            current.next = list2;
         }
         if(list2 == null){
-            mList.next = list1;
+            current.next = list1;
         }
+
         return dummy.next;
     }
 }
