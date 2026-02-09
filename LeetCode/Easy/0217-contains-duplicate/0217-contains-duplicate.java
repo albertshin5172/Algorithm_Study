11c1885ea26ae2d80cc32b1b3934405d9e61fa32
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-02-09 19:15:41 +0900

    9 ms (99.27%) 60.5 MB (99.94%) 02-09-2026 0217-contains-duplicate Easy .java

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,23 +1,17 @@
 class Solution {
+    //Time Complexity : Average O(n)
+    //Space Complexity : O(n)
     public boolean containsDuplicate(int[] nums) {
-        /*
-        Sort the array
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1]) return true;
-        }
+        //Create HashSet : Do not allow Duplicate
+        Set<Integer> set = new HashSet<>();
         
-        return false;   // If no duplicates found, return false
-        */
-        //Alternative solution solution using a HashSet to track visited numbers
-        Set<Integer> seen = new HashSet<>();
-        
-         // Iterate through each number in the array
-        for (int num : nums) {
-            if (!seen.add(num)) return true;    // if already exist, add will be false
+        for(int num : nums){
+            // Add() execution process
+            // 1.Calculate num's hashCode() -> Find the corresponding bucket location.
+            // 2.If the bucket doesn't contain the value, insert the value and return true.
+            // 3.If there is the duplicate value in the bucket, do not store the value and return false.
+            if(!set.add(num)) return true; 
         }
-
-        // If no duplicates encountered, return false
         return false;
     }
 }
