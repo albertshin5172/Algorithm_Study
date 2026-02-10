dd57d07b005f2e8105e726356827136f0ee913a9
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-02-10 16:25:32 +0900

    N/A (0%) N/A (0%) 02-10-2026 0234-palindrome-linked-list Easy .java

--- LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,20 +11,24 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         List<Integer> val = new ArrayList<>();
+        
+        // Store all node values in a list
         while(head != null){
             val.add(head.val);
             head = head.next;
         }
         
+        // Use two pointers to compare from both ends
         int left = 0;
         int right = val.size()-1;
         
         while(left < right){
-            if(val.get(left) != val.get(right)) return false;
+            if (!val.get(left).equals(val.get(right))) { return false;
 
             left++;
             right--;
         }
+        
         return true;
     }
 }
