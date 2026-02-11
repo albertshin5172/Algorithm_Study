428515f8948b952ae6d2963d0c97b2f1a0e71a01
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-02-11 21:54:30 +0900

    17 ms (17.6%) 46.8 MB (14.98%) 02-11-2026 0242-valid-anagram Easy .java

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        if (s.length() != t.length()) return false;
 
-        int[] count = new int[26];
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        for(char c : s.toCharArray()){
-            count[c - 'a']++;
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for(char c : t.toCharArray()){
-            count[c - 'a']--;
-            if(count[c-'a'] < 0 ) return false;
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) return false;
+
+            map.put(c, map.get(c) - 1);
+
+            if (map.get(c) < 0) return false;
         }
+
         return true;
     }
 }
