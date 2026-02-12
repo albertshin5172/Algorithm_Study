27aaea931017b20ec7c6ca1a8f4a376918bb2717
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-02-12 20:28:09 +0900

    1 ms (99.72%) 46.1 MB (96.91%) 02-12-2026 0002-add-two-numbers Medium .java

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.java
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,27 +10,44 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        // Dummy node to simplify list construction
         ListNode dummy = new ListNode();
-        ListNode cur=dummy;
-        int num = 0;
 
-        while(l1 != null || l2 != null || num != 0){
-            int sum = num;
-            
-            if(l1!=null){
+        // Pointer to build the result list
+        ListNode cur = dummy;
+
+        // Carry value from previous digit addition
+        int carry = 0;
+
+        // Continue while there is a digit to process or a remaining carry
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            // Add value from first list if available
+            if (l1 != null) {
                 sum += l1.val;
-                l1= l1.next;
+                l1 = l1.next;
             }
 
-            if(l2!=null){
+            // Add value from second list if available
+            if (l2 != null) {
                 sum += l2.val;
-                l2= l2.next;
+                l2 = l2.next;
             }
-          
-            cur.next = new ListNode(sum%10);
+
+            // Create new node with the current digit
+            cur.next = new ListNode(sum % 10);
+
+            // Move pointer forward
             cur = cur.next;
-            num = sum/10;
+
+            // Update carry for next iteration
+            carry = sum / 10;
         }
+
+        // Return the result list (skip dummy node)
         return dummy.next;
     }
 }
