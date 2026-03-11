c3d3efad6edace997a6c4048370c6e574b6a55b7
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-03-11 23:09:15 +0900

    10 ms (99.47%) 97.9 MB (9.62%) 03-11-2026 0217-contains-duplicate Easy .java

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,23 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        // HashSet is used to store unique elements
         HashSet<Integer> set = new HashSet<>();
 
-        for(int num : nums){
-            if(!set.add(num)) return true;
+        // Iterate through each number in the array
+        for (int num : nums) {
+            /*
+             * HashSet.add(value) returns:
+             * - true  : if the value was NOT already in the set (successfully added)
+             * - false : if the value already exists in the set
+             *
+             * If add() returns false, it means a duplicate is found
+             */
+            if (!set.add(num)) {
+                return true; // Duplicate detected
+            }
         }
 
+        // No duplicates found
         return false;
     }
 }
