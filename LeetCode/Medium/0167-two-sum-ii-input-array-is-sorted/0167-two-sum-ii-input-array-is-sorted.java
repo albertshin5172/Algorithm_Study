9f6515c3d3ebf9d807271abe5df85f7d2f7c3f43
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-07-03 00:00:21 +0900

    2 ms (95.93%) 47.7 MB (99.54%) 07-03-2026 0167-two-sum-ii-input-array-is-sorted Medium .java

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,15 +1,27 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        // Two pointers starting from both ends.
         int left = 0;
-        int right = numbers.length-1;
+        int right = numbers.length - 1;
 
-        while(left<right){
-            int sum = numbers[left]+numbers[right];
-            if(sum == target) return new int[] {left+1, right+1};
-            else if(sum < target) left++;
-            else right--;
+        while (left < right) {
+            // Calculate the sum of the two current values.
+            int sum = numbers[left] + numbers[right];
+
+            // Target found.
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            }
+
+            if (sum < target) { // Increase the sum by moving the left pointer.
+                left++;
+            }else {  // Decrease the sum by moving the right pointer.
+                right--;
+            }
         }
 
+        // This line is never reached because
+        // the problem guarantees exactly one solution.
         return new int[] {};
     }
 }
