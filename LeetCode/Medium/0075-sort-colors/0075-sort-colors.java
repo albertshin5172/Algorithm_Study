f7c4fe36aee54eef5ac961ac0ca5350fe96fa645
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-07-05 17:29:15 +0900

    0 ms (100%) 43.4 MB (60%) 07-05-2026 0075-sort-colors Medium .java

--- LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
+
+        // left: next position for 0
+        // mid: current element being examined
+        // right: next position for 2
         int left = 0;
         int mid = 0;
         int right = nums.length - 1;
-        
-        while(mid <= right){
-            if(nums[mid] == 0){
+
+        // Process elements until the mid pointer passes the right pointer.
+        while (mid <= right) {
+
+            // Place 0 at the beginning of the array.
+            if (nums[mid] == 0) {
                 swap(nums, left, mid);
                 left++;
                 mid++;
-            }else if(nums[mid] == 1){
+            }
+
+            // 1 is already in the correct middle section.
+            else if (nums[mid] == 1) {
                 mid++;
-            }else{
+            }
+
+            // Place 2 at the end of the array.
+            else {
                 swap(nums, mid, right);
                 right--;
+
+                // Do NOT move mid here.
+                // The swapped element has not been processed yet.
             }
         }
-        
     }
 
-    public void swap(int[] arr, int i, int j){
+    // Swap two elements in the array.
+    public void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
