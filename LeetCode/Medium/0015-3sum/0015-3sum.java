d07e50ad6d41649ee03df1ffaee988c03576a0f9
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-07-06 17:17:51 +0900

    33 ms (73.89%) 59.4 MB (32.88%) 07-06-2026 0015-3sum Medium .java

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,39 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // Sort the input array to make two-pointer approach possible
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
-        // Iterate through the array, fixing one element at a time
-        for (int i=0; i<nums.length-2; i++) {
-            
-            // Skip duplicate elements for the first number
-            if (i>0 && nums[i] == nums[i-1]) continue;
+        for(int i = 0; i < nums.length-2; i++){
+           if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i+1;               // Pointer to the next element
-            int right = nums.length-1;    // Pointer to the last element
-
-            // Use two pointers to find pairs that sum with nums[i] to zero
-            while (left<right) {
+            int left = i+1;
+            int right = nums.length-1;
+            while(left<right){
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum==0) {
-                    // Found a triplet that sums to zero
+                if(sum == 0){
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
 
-                    // Skip duplicates for the second and third numbers
-                    while (left<right && nums[left] == nums[left-1]) left++;
-                    while (left<right && nums[right] == nums[right+1]) right--;
-                } else if (sum<0) {
-                    left++; // Move left pointer right to increase the sum
-                } else {
-                    right--; // Move right pointer left to decrease the sum
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }else if(sum < 0){
+                    left++; 
+                }else{
+                    right--;
                 }
             }
         }
-
         return result;
     }
 }
