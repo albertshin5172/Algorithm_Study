c38229aa7c993a952ab6208047a8f32b0f25096f
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-07-09 22:18:22 +0900

    0 ms (100%) 43 MB (35.37%) 07-09-2026 0876-middle-of-the-linked-list Easy .java

--- LeetCode/Easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ LeetCode/Easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,14 +10,21 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
+        // Slow pointer moves one step at a time.
         ListNode slow = head;
+        // Fast pointer moves two steps at a time.
         ListNode fast = head;
-        
-        while(fast != null && fast.next != null){
+
+        // Move fast pointer two nodes and slow pointer one node
+        // until fast reaches the end of the linked list.
+        while (fast != null && fast.next != null) {
+
             fast = fast.next.next;
             slow = slow.next;
         }
 
+        // When fast reaches the end,
+        // slow will be pointing to the middle node.
         return slow;
     }
 }
