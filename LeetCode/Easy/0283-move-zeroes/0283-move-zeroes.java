0c71a81ed17094a412884d688b9d9751d24b809f
SeungHun Shin <32608100+albertshin5172@users.noreply.github.com>
2026-08-04 22:20:05 +0900

    2 ms (92.13%) 47.8 MB (57.74%) 08-04-2026 0283-move-zeroes Easy .java

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,26 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        //The keypoint is O(n) / in-place / one-pass
-        //Position where non-zero elements will be placed
+        // Pointer for the next position to place a non-zero element
         int idx = 0;
+        // Counter to keep track of the number of zeroes encountered
+        int zeroCount = 0;
 
-        for(int i=0; i<nums.length; i++){
-            // Swap non-zero elements forward
-            if(nums[i] != 0){
-                int tmp = nums[idx];
-                nums[idx] = nums[i];
-                nums[i] = tmp;
-                idx++;
+        // First pass: Move all non-zero elements to the front of the array
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // Place the non-zero element at the current 'idx' and increment it
+                nums[idx++] = nums[i]; 
+            } else {
+                // Count the number of zeroes to be appended later
+                zeroCount++;
             }
+        }
+
+        // Second pass: Fill the remaining positions at the end with zeroes
+        // Using 'zeroCount' to determine how many zeroes to fill
+        for (int i = 0; i < zeroCount; i++) {
+            // Fill from the very last index backwards
+            nums[nums.length - 1 - i] = 0;
         }
     }
 }
